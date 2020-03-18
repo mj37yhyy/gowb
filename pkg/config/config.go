a1e3f7190a263bf3a80a9e812f3451b532f1c4aa
miaojia <mj37yhyy@yeah.net>
2020-03-18 14:32:14 +0800

    config.Config添加yaml和json支持

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,42 +3,42 @@ package config
 import "time"
 
 type Config struct {
-	Log   Log   `mapstructure:"log"`
-	Web   Web   `mapstructure:"web"`
-	Trace Trace `mapstructure:"trace"`
-	Mysql Mysql `mapstructure:"mysql"`
+	Log   Log   `mapstructure:"log" yaml:"log" json:"log"`
+	Web   Web   `mapstructure:"web" yaml:"web" json:"web"`
+	Trace Trace `mapstructure:"trace" yaml:"trace" json:"trace"`
+	Mysql Mysql `mapstructure:"mysql" yaml:"mysql" json:"mysql"`
 }
 
 type Fields struct {
-	Name  string `mapstructure:"name"`
-	Value string `mapstructure:"value"`
-	Ref   string `mapstructure:"ref"`
+	Name  string `mapstructure:"name" yaml:"name" json:"name"`
+	Value string `mapstructure:"value" yaml:"value" json:"value"`
+	Ref   string `mapstructure:"ref" yaml:"ref" json:"ref"`
 }
 
 type Log struct {
-	Level       string   `mapstructure:"level"`
-	Formatter   string   `mapstructure:"formatter"`
-	PrintMethod bool     `mapstructure:"printMethod"`
-	Fields      []Fields `mapstructure:"fields"`
+	Level       string   `mapstructure:"level" yaml:"level" json:"level"`
+	Formatter   string   `mapstructure:"formatter" yaml:"formatter" json:"formatter"`
+	PrintMethod bool     `mapstructure:"printMethod" yaml:"printMethod" json:"printMethod"`
+	Fields      []Fields `mapstructure:"fields" yaml:"fields" json:"fields"`
 }
 
 type Web struct {
-	Port    int    `mapstructure:"port"`
-	RunMode string `mapstructure:"runMode"`
+	Port    int    `mapstructure:"port" yaml:"port" json:"port"`
+	RunMode string `mapstructure:"runMode" yaml:"runMode" json:"runMode"`
 }
 
 type Trace struct {
-	Fields []string `mapstructure:"fields"`
+	Fields []string `mapstructure:"fields" yaml:"fields" json:"fields"`
 }
 
 type Mysql struct {
-	UserName        string        `mapstructure:"userName"`
-	Password        string        `mapstructure:"password"`
-	Host            string        `mapstructure:"host"`
-	Port            string        `mapstructure:"port"`
-	Database        string        `mapstructure:"database"`
-	Params          string        `mapstructure:"params"`
-	MaxOpenConns    int           `mapstructure:"maxOpenConns"`
-	MaxIdleConns    int           `mapstructure:"maxIdleConns"`
-	ConnMaxLifetime time.Duration `mapstructure:"connMaxLifetime"`
+	UserName        string        `mapstructure:"userName" yaml:"userName" json:"userName"`
+	Password        string        `mapstructure:"password" yaml:"password" json:"password"`
+	Host            string        `mapstructure:"host" yaml:"host" json:"host"`
+	Port            string        `mapstructure:"port" yaml:"port" json:"port"`
+	Database        string        `mapstructure:"database" yaml:"database" json:"database"`
+	Params          string        `mapstructure:"params" yaml:"params" json:"params"`
+	MaxOpenConns    int           `mapstructure:"maxOpenConns" yaml:"maxOpenConns" json:"maxOpenConns"`
+	MaxIdleConns    int           `mapstructure:"maxIdleConns" yaml:"maxIdleConns" json:"maxIdleConns"`
+	ConnMaxLifetime time.Duration `mapstructure:"connMaxLifetime" yaml:"connMaxLifetime" json:"connMaxLifetime"`
 }
