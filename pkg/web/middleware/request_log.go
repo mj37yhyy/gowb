08522723cac4501e5917881479383294ad579522
程锦 <709192853@qq.com>
2020-05-12 10:02:07 +0800

    增加request_log middleware

--- pkg/web/middleware/request_log.go
+++ pkg/web/middleware/request_log.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/mj37yhyy/gowb/pkg/model"
 	logger "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"time"
@@ -28,10 +30,20 @@ func RequestInLog(c *gin.Context) {
 // 请求输出日志
 func RequestOutLog(c *gin.Context) {
 	endExecTime := time.Now()
-	response, _ := c.Get("response")
+	//response, _ := c.Get("response")
 	st, _ := c.Get("startExecTime")
 
 	startExecTime, _ := st.(time.Time)
+
+	var response = model.NewResponse()
+	blw := &bodyLogWriter{
+		body:           bytes.NewBufferString(""),
+		ResponseWriter: c.Writer,
+	}
+	c.Writer = blw
+	if err := json.Unmarshal(blw.body.Bytes(), &response); err != nil {
+		logger.Println(err, "response body can not unmarshal to model.Response struct, body: `%s`", blw.body.Bytes())
+	}
 	var logFields = logger.Fields{
 		"uri":       c.Request.RequestURI,
 		"method":    c.Request.Method,
